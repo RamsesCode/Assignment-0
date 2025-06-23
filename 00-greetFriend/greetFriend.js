6e64786c29aa904399bc5aca0bcfba7f12a5c572
RamsesCode <RamsesSanchez25@gmail.com>
2025-06-22 23:09:26 -0400

    greet friend passed tests

--- 00-greetFriend/greetFriend.js
+++ 00-greetFriend/greetFriend.js
@@ -1,6 +1,9 @@
 function greetFriend(name) {
-  // Insert code here;
+  if(typeof name === 'string')
+    return `Hello, ${name}!`;
 }
 
 // Do not edit this line;
 module.exports = greetFriend;
+
+greetFriend(); 
